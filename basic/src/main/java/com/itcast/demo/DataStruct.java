baa6f99d83ed79bb2a1c88a77a342a79f19c85bc
treey.qian <treey.qian@vipshop.com>
2017-04-20 16:30:29 +0800

    项目构建

---
+++ basic/src/main/java/com/itcast/demo/DataStruct.java
@@ -0,0 +1,7 @@
+package com.itcast.demo;
+
+/**
+ * Created by treey.qian on 2017/4/20.
+ */
+public class DataStruct {
+}
---
+++ basic/src/test/java/com/basic/test/qfl/ApplocationTest.java
@@ -0,0 +1,15 @@
+package com.basic.test.qfl;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplocationTest {
+
+    @Test
+    public void testMethod() {
+
+    }
+
+}
