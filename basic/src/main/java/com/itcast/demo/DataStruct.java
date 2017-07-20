f5c7bb1285b9da1937c478fc844294a7c44331f5
treey.qian <treey.qian@vipshop.com>
2017-07-20 16:15:03 +0800

    code

--- basic/src/main/java/com/itcast/demo/DataStruct.java
+++
@@ -1,7 +0,0 @@
-package com.itcast.demo;
-
-/**
- * Created by treey.qian on 2017/4/20.
- */
-public class DataStruct {
-}
---
+++ basic/src/test/java/com/itcast/basic/test/qfl/TestService.java
@@ -0,0 +1,20 @@
+package com.itcast.basic.test.qfl;
+
+import org.junit.Test;
+
+/**
+ * Created by treey.qian on 2017/7/13.
+ */
+public class TestService {
+
+    @Test
+    public void testFind() {
+
+        try {
+            System.out.println("hello world");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
