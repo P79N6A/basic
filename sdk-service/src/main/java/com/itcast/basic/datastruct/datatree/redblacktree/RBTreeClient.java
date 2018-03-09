6b9f777d440c25f26f4888c62631624928966bd9
treey.qian <treey.qian@vipshop.com>
2018-03-09 20:54:33 +0800

    完善红黑树

--- sdk-service/src/main/java/com/itcast/basic/datastruct/datatree/redblacktree/RBTreeClient.java
+++ sdk-service/src/main/java/com/itcast/basic/datastruct/datatree/redblacktree/RBTreeClient.java
@@ -8,8 +8,14 @@ public class RBTreeClient {
     public static void main(String[] args) {
         try {
             System.out.println("enter into RBTreeClient");
-
-
+            RBTree<Integer> rbTree=new RBTree();
+            rbTree.addNode(5);
+            rbTree.addNode(20);
+            rbTree.addNode(4);
+            rbTree.addNode(12);
+//            rbTree.addNode(10);
+//            rbTree.addNode(15);
+            rbTree.preDisplay();
         } catch (Exception e) {
             e.printStackTrace();
         }
