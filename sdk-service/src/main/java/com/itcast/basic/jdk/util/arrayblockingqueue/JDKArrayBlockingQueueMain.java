b456df29f23efe6e5ec2fb1e52ad7a27f5897a4d
treey.qian <treey.qian@vipshop.com>
2018-03-30 19:14:24 +0800

    添加 阻塞队列

---
+++ sdk-service/src/main/java/com/itcast/basic/jdk/util/arrayblockingqueue/JDKArrayBlockingQueueMain.java
@@ -0,0 +1,15 @@
+package com.itcast.basic.jdk.util.arrayblockingqueue;
+
+/**
+ * Created by treey.qian on 2018/3/30.
+ */
+public class JDKArrayBlockingQueueMain {
+    public static void main(String[] args) {
+        try {
+            System.out.println("enter into JDKArrayBlockingQueueMain");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
