141929da2b5356626fa0d26ff7ebdb1668878dfa
treey.qian <treey.qian@vipshop.com>
2018-03-31 14:34:32 +0800

    添加阻塞双向队列

---
+++ sdk-service/src/main/java/com/itcast/basic/jdk/util/linkedblockingdeque/LinkedBlockingDequeMain.java
@@ -0,0 +1,15 @@
+package com.itcast.basic.jdk.util.linkedblockingdeque;
+
+/**
+ * Created by treey.qian on 2018/3/31.
+ */
+public class LinkedBlockingDequeMain {
+    public static void main(String[] args) {
+        try {
+            System.out.println("enter into LinkedBlockingDequeMain");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
