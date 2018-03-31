0123ca36fb1b540e23c6193a17e0ed1187a7a592
qfl <1258166167@qq.com>
2018-03-31 16:55:38 +0800

    阻塞队列

---
+++ sdk-service/src/main/java/com/itcast/basic/jdk/util/linkedblockingqueue/JDKLinkedBlockingQueue.java
@@ -0,0 +1,7 @@
+package com.itcast.basic.jdk.util.linkedblockingqueue;
+
+/**
+ * Created by qfl on 2018/3/30.
+ */
+public class JDKLinkedBlockingQueue {
+}
---
+++ sdk-service/src/main/java/com/itcast/basic/jdk/util/linkedblockingqueue/JDKLinkedBlockingQueueMain.java
@@ -0,0 +1,14 @@
+package com.itcast.basic.jdk.util.linkedblockingqueue;
+
+/**
+ * Created by qfl on 2018/3/30.
+ */
+public class JDKLinkedBlockingQueueMain {
+    public static void main(String[] args) {
+        try {
+          System.out.println("enter into JDKLinkedBlockingQueueMain");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
