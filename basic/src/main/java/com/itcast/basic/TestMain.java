42c89837ef8334f18dce3233caa71be2a2e2e43e
qfl <1258166167@qq.com>
2018-05-23 22:59:35 +0800

    添加测试方法

---
+++ basic/src/main/java/com/itcast/basic/TestMain.java
@@ -0,0 +1,15 @@
+package com.itcast.basic;
+
+/**
+ * Created by Administrator on 2018/5/23.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        try {
+            int i = 'I';
+            System.out.println("enter into I=" + i);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- sdk-service/src/main/java/com/itcast/basic/TestStatic.java
+++ sdk-service/src/main/java/com/itcast/basic/TestStatic.java
@@ -1,5 +1,10 @@
 package com.itcast.basic;
 
+import org.springframework.context.ApplicationContextAware;
+import sun.text.normalizer.UTF16;
+
+import java.nio.charset.Charset;
+
 /**
  * Created by treey.qian on 2018/3/27.
  */
@@ -7,6 +12,7 @@ public class TestStatic {
     public static void main(String[] args) {
         try {
             System.out.println("enter into TestStatic ");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
